ab5cfeb92372826a66cb913243c43af512749273
ravi <rprakash262@gmail.com>
2022-12-08 11:32:29 +0530

    dsb

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORTT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORTT}`));
