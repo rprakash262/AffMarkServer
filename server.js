16e9adad4a01f3852bd5ee6f9875e1765c731c29
ravi <rprakash262@gmail.com>
2022-12-13 20:52:03 +0530

    skdb

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const PORTT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORTT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
